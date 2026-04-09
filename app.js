0640616db1711a8e10d61cf07608ea270f6ef580
Erick752Vini <erickviniciusrocha80@gmail.com>
2026-04-09 14:25:03 -0300

    finalizando api

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+const userRoutes = require('./routes/users');
 const userRoutes = require('./routes/corredores');
+const userRoutes = require('./routes/voltas');
+app.use('/user', userRoutes);
 app.use('/corredores', userRoutes);
+app.use('/voltas', userRoutes);
 
 
 module.exports = app;
