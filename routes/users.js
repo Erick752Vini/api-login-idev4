59465e8eba20e6be84491d7858f5e9ec70c2970a
Erick752Vini <erickviniciusrocha80@gmail.com>
2026-04-23 15:13:16 -0300

    user att

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,11 @@ routes.get('/', (req, res) => {
     });
 });
 
-routes.post('/z', (req, res) => {
-    const { nome, email } = req.body; 
+routes.post('/create', async (req, res) => {        
+    const { nome, email, senha } = req.body; 
+    const senhaHash = await bcrypt.hash(senha, 10); 
 
-    db.query('INSERT INTO users (nome, email) VALUES (?, ?)', [nome, email], (err, result) => {
+    db.query('INSERT INTO users (nome, email, senha) VALUES (?, ?, ?)', [nome, email, senhaHash], (err, result) => { 
         if (err) {
             return res.status(500).json({ error: 'Erro ao criar usuário' });
         }
@@ -23,7 +24,7 @@ routes.post('/z', (req, res) => {
     });
 });
 
-routes.put('edit/:id', (req, res) => {
+routes.put('/edit/:id', (req, res) => {               
     const { id } = req.params; 
     const { nome, email } = req.body; 
 
@@ -40,7 +41,7 @@ routes.put('edit/:id', (req, res) => {
     });
 });
 
-routes.delete('delete/:id', (req, res) => {
+routes.delete('/delete/:id', (req, res) => {         
     const { id } = req.params; 
 
     db.query('DELETE FROM users WHERE id = ?', [id], (err, result) => {
