4cc1e32a22b8e6fb238739c41e5f53d012ae2d34
Erick752Vini <erickviniciusrocha80@gmail.com>
2026-05-11 11:15:15 -0300

    att

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static('C:/temp/Erick/backend-integrador/frontend'));
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(express.json());
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'frontend')));
 const userRoutes = require('./routes/users');
-//const userRoutes = require('./routes/corredores');
-//const userRoutes = require('./routes/voltas');
-app.use('/usuarios', userRoutes);
-//app.use('/corredores', userRoutes);
-//app.use('/voltas', userRoutes);
+const corredoresRoutes = require('./routes/corredores');
+const voltasRoutes = require('./routes/voltas');
 const geradorRoutes = require('./routes/gerador');
+
+app.use('/usuarios', userRoutes);
+app.use('/corredores', corredoresRoutes);
+app.use('/voltas', voltasRoutes);
 app.use('/gerador', geradorRoutes);
 
 module.exports = app;
